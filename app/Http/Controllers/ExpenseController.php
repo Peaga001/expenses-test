e4b48f0a35cfc61588e0b73c19869ab658e88223
Pedro Henrique <phalmeida001@gmail.com>
2024-01-25 03:01:21 -0300

    feat:desenvolvimento da controller.

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Requests
+use Illuminate\Auth\Access\AuthorizationException;
+use App\Http\Requests\Expense\{
+    Update,
+    Store
+};
+
+//Resources
+use App\Http\Resources\ExpenseResource;
+
+//Models
+use App\Models\Expense;
+
+//Miscellaneous
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ExpenseController extends Controller
+{
+    public function index(): object
+    {
+        $expenses = Expense::all();
+
+        return response(
+            content: [
+                'message' => 'Busca realizada com sucesso!',
+                'data'    => ExpenseResource::collection($expenses)
+            ]
+        )->setStatusCode(
+            code: Response::HTTP_OK
+        );
+    }
+
+    public function store(Store $request): object
+    {
+        $expense = Expense::query()->make($request->toArray());
+        $expense->save();
+
+        return response(
+            content: [
+                'message' => 'Despesa criada com sucesso!',
+                'data'    => new ExpenseResource($expense)
+            ]
+        )->setStatusCode(
+            code: Response::HTTP_CREATED
+        );
+    }
+
+    public function show(string $id): object
+    {
+        try {
+
+            $expense = Expense::query()->findOrFail($id);
+
+            $this->authorize('view', $expense);
+
+            return response(
+                content: [
+                    'message' => 'Despesa encontrada com sucesso!',
+                    'data'    => new ExpenseResource($expense)
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_OK
+            );
+
+        }catch (ModelNotFoundException){
+            return response(
+                content: [
+                    'message' => 'Despesa não encontrada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_NOT_FOUND
+            );
+        } catch (AuthorizationException) {
+            return response(
+                content: [
+                    'message' => 'Ação não autorizada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_UNAUTHORIZED
+            );
+        }
+    }
+
+    public function update(Update $request, string $id): object
+    {
+        try {
+
+            $expense = Expense::query()->findOrFail($id);
+
+            $this->authorize('view', $expense);
+
+            $expense->fill($request->all())->save();
+
+            return response(
+                content: [
+                    'message' => 'Despesa atualizada com sucesso!',
+                    'data'    => new ExpenseResource($expense)
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_OK
+            );
+
+        }catch (ModelNotFoundException){
+            return response(
+                content: [
+                    'message' => 'Despesa não encontrada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_NOT_FOUND
+            );
+        } catch (AuthorizationException) {
+            return response(
+                content: [
+                    'message' => 'Ação não autorizada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_UNAUTHORIZED
+            );
+        }
+    }
+
+    public function destroy(string $id): object
+    {
+        try {
+
+            $expense = Expense::query()->findOrFail($id);
+            $this->authorize('view', $expense);
+
+            $expense->delete();
+
+            return response(
+                content: [
+                    'message' => 'Despesa removida com sucesso!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_OK
+            );
+
+        }catch (ModelNotFoundException){
+            return response(
+                content: [
+                    'message' => 'Despesa não encontrada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_NOT_FOUND
+            );
+        } catch (AuthorizationException) {
+            return response(
+                content: [
+                    'message' => 'Ação não autorizada!'
+                ]
+            )->setStatusCode(
+                code: Response::HTTP_UNAUTHORIZED
+            );
+        }
+    }
+}
